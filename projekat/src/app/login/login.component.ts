219d7436c75c7c1b1f067763eb9b9eb9658dad75
Aleksa Matic <aleksa.matic408@gmail.com>
2021-04-11 17:44:28 +0200

    login page doesnt show navigation and sidebar, added angular form for login

--- projekat/src/app/login/login.component.ts
+++ projekat/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +9,27 @@ import { Component, OnInit, Input } from '@angular/core';
 export class LoginComponent implements OnInit {
   @Input() title!: string;
 
+  loginForm!: FormGroup;
+
   constructor() {
     
    }
 
-  ngOnInit(): void {
-
+   ngOnInit(): void {
+    this.initForm();
   }
 
+  private initForm() {
+    this.loginForm = new FormGroup({
+      'username': new FormControl(''),
+      'password': new FormControl('')
+    });
+  }
+
+  onSubmit() {
+    //validacija da li je username in use, da li su passwordi isti itd
+    console.log(this.loginForm.get('username')?.value);
+    console.log(this.loginForm.value);
+    console.log(this.loginForm);
+  }
 }

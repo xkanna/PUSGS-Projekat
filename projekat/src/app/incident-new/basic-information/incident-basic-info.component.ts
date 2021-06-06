2b7d75b2fb217613375ec1d70154f3ed0d8589f0
Aleksa Matic <aleksa.matic408@gmail.com>
2021-06-06 18:54:51 +0200

    fixed basic info bindings

--- projekat/src/app/incident-new/basic-information/incident-basic-info.component.ts
+++ projekat/src/app/incident-new/basic-information/incident-basic-info.component.ts
@@ -13,25 +13,27 @@ export class IncidentBasicInfoComponent implements OnInit {
   registerForm!: FormGroup;
 
   public inc!:Incident
+  // public description!:string;
+  // public voltage!:number;
+  // public calls!:number;
+  // public confirmed!:boolean;
+  // public priority!:number;
+  // public selectedType!:string;
+  // public affectedCustomers!:number;
+  // public ata:Date = new Date();
+  // public eta:Date = new Date();
+  // public etr:Date = new Date();
+  // public outageTime:Date = new Date();
+  // public scheduledTime:Date = new Date();
 
   constructor(private service:IncidentService) {  }
 
   ngOnInit(): void {
     this.inc = this.service.currentIncident;//curr incident mora da se reset, vrv kad se klikne new ili mozda edit ako imamo
-    this.initForm();
   }
 
-  private initForm() {
-    this.registerForm = new FormGroup({
-      'date': new FormControl(''),
-      'date1': new FormControl(''),
-      'voltage': new FormControl(''),
-      'description': new FormControl(''),
-      'date2': new FormControl(''),
-      'date3': new FormControl(''),
-      'date4': new FormControl(''),
-
-    });
+  ngOnDestroy():void{
+    this.service.currentIncident = this.inc;
   }
 
 }
